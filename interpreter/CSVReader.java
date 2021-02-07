e509e776b6a241829547bd2dfc0cce4265f70593
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2021-02-06 21:27:39 -0500

    Updated reader
    Reader now reads from CSV's and populates ArrayList<Player>

--- interpreter/CSVReader.java
+++ interpreter/CSVReader.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
  
 public class CSVReader {
   
-public void scanCSV(String path) {
+  private ArrayList<Player> playerList;
+  
+  public CSVReader() {
+    playerList = new ArrayList<Player>();
+  }
+  
+public void printCSV(String path) {
   Scanner sc = null;
   try {
     sc = new Scanner(new File(path));
@@ -19,6 +25,32 @@ public void scanCSV(String path) {
   System.out.print(sc.next());  //find and returns the next complete token from this scanner  
   }   
   sc.close();  //closes the scanner  
+  }
+
+public void readCSV(String path) {
+  
+  Scanner sc = null;
+  try {
+    sc = new Scanner(new File(path));
+  } catch (FileNotFoundException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }  
+  sc.useDelimiter(",");   //sets the delimiter pattern  
+  while (sc.hasNext())  //returns a boolean value  
+  { 
+    String name = new String();
+    name = sc.next();  //find and returns the next complete token from this scanner  
+    Player newPlayer = new Player(name);
+    playerList.add(newPlayer);
+
+  }   
+  sc.close();  //closes the scanner  
+
+}
+
+  public ArrayList<Player> getPlayerList(){
+    return playerList;
   }
 
 }
