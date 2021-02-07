30fe188fec5fa057079eda9f5959301b4ef3e52b
Justinding765 <56009508+Justinding765@users.noreply.github.com>
2021-02-06 20:39:47 -0500

    Add files via upload

--- Player_Data_Base/src/player_Info_At_Moment.java
+++ Player_Data_Base/src/player_Info_At_Moment.java
@@ -5,6 +5,7 @@ public class player_Info_At_Moment {
 	private final int ranking;
 	private final int age;
 	private final String date;
+	private final double date_Formula;
 	
 	public player_Info_At_Moment( int ranking,
 			int age, String date) 
@@ -13,7 +14,18 @@ public class player_Info_At_Moment {
 		this.ranking = ranking;
 		this.age = age;
 		this.date = date;
+		this.date_Formula = date_Formula(date);
 		
 	
+	}
+	private double date_Formula(String date) {
+		double value;
+		String temp[];
+		temp = date.split(",");
+		for(int i = 0;i<3;i++) 
+		{
+			value+= 
+		}		
+		
 	}
 }
--- Player_Data_Base/src/player_Total_Info.java
+++ Player_Data_Base/src/player_Total_Info.java
@@ -8,6 +8,7 @@ public class player_Total_Info {
 	private  String firstName;
 	private  String lastName;
 	private  String nationality;
+	private double potential_Formula;
 	
 	public player_Total_Info(String firstName, String lastName, String nationality) {
 		this.firstName = firstName;
@@ -15,9 +16,12 @@ public class player_Total_Info {
 		this.nationality = nationality;
 		
 	}
-	public void add_Info(player_Info_At_Moment data) {
+	public void add_Info(int ranking, int age, String date) {
+		player_Info_At_Moment data = new player_Info_At_Moment(ranking, age, date);
 		player.add(data);
 		
+		
 	}
+	//private calculate_potential()
 
 }
